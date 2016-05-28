820af68d36cd1eace68e6d84656baa5727e2e873
Javier Ibarrola <ibarroladt@gmail.com>
2016-05-27 20:03:12 -0500

    adds not found message on twitter search

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -3,7 +3,7 @@ class TwitterUsersController < ApplicationController
 
   def search_users
     @search_word = params[:twitter][:search]
-    @users = CLIENT.user_search(@search_word)  
+    @users = CLIENT.user_search(@search_word) 
   end
 
 end
