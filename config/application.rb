76b5881cd99d749a3372573102d797aa222041bc
Javier Ibarrola <ibarroladt@gmail.com>
2016-05-31 12:45:23 -0500

    fixes secrets

--- config/application.rb
+++ config/application.rb
@@ -32,13 +32,14 @@ module CodeaTAG
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'twitter_secrets.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
-end
-
-env_config = YAML.load_file(APP_ROOT.join('config', 'twitter_secrets.yaml'))
-
-env_config.each do |key, value|
-  ENV[key] = value
 end
 
 # Aquí es donde vamos a pegar el TwitterClient 
